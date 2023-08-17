beb1f754b4700d0c3f8ef93aa2571a7cb46ca1d4
Gadmani Raida <78973142+Raida95@users.noreply.github.com>
2023-08-17 14:51:01 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-
+  
+  client_secret   = "20f4b2a1-b33a-d957-aa8a-2ff8204e9de4"
   tenant_id       = "7ff97941-b6ff-4369-a153-efd4d007f873"
-  features        {}
-}
+  subscription_id = "f604f5c5-545d-482b-89b4-
